007b338cd47a2ce4a97e6e58f850566bfcea28ac
manelyset <julivel1607@gmail.com>
2023-10-15 15:56:07 +0600

    feat: controller and team randomizing

---
+++ app/controllers/tours_controller.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class ToursController < ApplicationController
+  def index; end
+end
---
+++ app/services/teams/list_generator_service.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Teams
+  class ListGeneratorService
+    NUMBER_OF_TEAMS = 16
+
+    class << self
+      def call(teams_list)
+        team_names_array = teams_list.split('\n').first(NUMBER_OF_TEAMS)
+        fill_missing(team_names_array) if team_names_array.length < NUMBER_OF_TEAMS
+        team_names_array
+      end
+
+      private
+
+      def fill_missing(team_names_array)
+        index = team_names_array.length + 1
+
+        while index <= NUMBER_OF_TEAMS
+          team_names_array << "Team #{index}"
+          index += 1
+        end
+
+        team_names_array
+      end
+    end
+  end
+end
